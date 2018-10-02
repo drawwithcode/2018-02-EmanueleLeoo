d3f53ce989847199671efff5adb7e32b3dcd9c7d
Emanuele Leonardi <emanuele.leonardi97@gmail.com>
2018-10-02 18:57:59 +0200

    New Code

--- sketch.js
+++ sketch.js
@@ -4,8 +4,43 @@ function preload(){
 
 function setup() {
   // put setup code here
+  createCanvas(windowWidth, windowHeight);
+  background(40);
+  angleMode(DEGREES);
+
+  frameRate(32);
+
 }
 
 function draw() {
   // put drawing code here
+  translate(width/2, height/2);
+  noFill();
+
+  stroke(150, 40, 40);
+  line(175, 0,cos(frameCount*3)*175,sin(frameCount*3)*175);
+
+  rotate(frameCount*3);
+
+  stroke(150, 40, 40);
+  line(cos(frameCount*3)*175, 175, 0, sin(frameCount*3)*175);
+
+  stroke(150, 40, 40);
+  line(175, 0,cos(frameCount*3)*175,sin(frameCount*3)*175);
+
+  translate(cos(frameCount*8)*5,height/4);
+  noFill();
+  ellipse(-300, 0, 185, 185);
+
+  stroke(230, 110, 76);
+  fill(230, 110, 76, 35);
+  translate(width/8,height/8);
+  noStroke();
+  ellipse(-200, 0, cos(frameCount*8)*175, sin(frameCount*8)*175);
+
+  // Stops the draw function if a certain condition avaluates true
+  if (frameCount == 240) {
+    noLoop();
+  }
+
 }
